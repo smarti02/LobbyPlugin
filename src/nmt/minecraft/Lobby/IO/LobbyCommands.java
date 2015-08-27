d4d010a64c29092b5cacc9fe02bcf29bdcfc5595
Stephanie Martinez <smarti02@nmt.edu>
2015-08-27 15:11:40 -0600

    added help menu to commands

--- src/nmt/minecraft/Lobby/IO/LobbyCommands.java
+++ src/nmt/minecraft/Lobby/IO/LobbyCommands.java
@@ -13,7 +13,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class LobbyCommands implements CommandExecutor{
-	private static String[] commandList = {"setEntrance", "setExit", "setEnteranceLocation", "setExitLocation", "open", "close", "newLobby", "removeLobby", "help"}; //if you change this remember to add it to the help function	
+	private static String[] commandList = {"setEntrance", "setExit", "setentranceLocation", 
+			"setExitLocation", "open", "close", "newLobby", "removeLobby", "help"}; //if you change this remember to add it to the help function	
 	
 	private String aquaChat = ChatColor.AQUA+"";
 	private String blueChat = ChatColor.BLUE+"";
@@ -54,6 +55,61 @@ public class LobbyCommands implements CommandExecutor{
 		if (!(sender instanceof Player)) {
 			sender.sendMessage("You must be a player to use this command!");
 			return false;
+		}
+
+		if(args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("help")== false)){
+			sender.sendMessage("Something went wrong... We need more arguments");
+			sender.sendMessage("Valid commands are "+commandsToString()+"\n");
+		}
+		
+		//The help menu
+		if (args[0].equalsIgnoreCase("help")) {
+			sender.sendMessage(greenChat + boldChat + "===Help===" + resetChat);
+			sender.sendMessage("Usage: " + redChat + "/lobby " + blueChat + "[command] " + goldChat + "[command arguments]" + resetChat);
+			sender.sendMessage(greenChat + boldChat + "===Commands===" + resetChat);
+			sender.sendMessage(blueChat + "setExitLocation " + goldChat + " [lobby name]" + resetChat + " Sets the location the user will teleport to when they exit the lobby");
+			sender.sendMessage(blueChat + "setEntranceLocation " + goldChat + " [lobby name]" + resetChat + " Sets the location the user will teleport to when they enter the lobby");
+			sender.sendMessage(blueChat + "setExit " + goldChat + " [lobby name]" + resetChat + " Sets the location of the entrance button");
+			sender.sendMessage(blueChat + "setEntrance " + goldChat + " [lobby name]" + resetChat + " Sets the location the exit button");
+			sender.sendMessage(blueChat + "start " + goldChat + " [lobby name]" + resetChat + " allows users to begin entering the lobby");
+			sender.sendMessage(blueChat + "stop " + goldChat + " [lobby name]" + resetChat + " kicks all current lobby players and prevents more from joining");
+			sender.sendMessage(blueChat + "newLobby " + goldChat + " [lobby name]" + resetChat + " creates a new lobby");
+			sender.sendMessage(blueChat + "removeLobby " + goldChat + " [lobby name]" + resetChat + " removes the given lobby");
+		}
+		
+		else if(args[0].equalsIgnoreCase("setExitLocation")){
+			
+		}
+		
+		else if(args[0].equalsIgnoreCase("setEntranceLocation")){
+			
+		}
+		
+		else if(args[0].equalsIgnoreCase("setExit")){
+			
+		}
+		
+		else if(args[0].equalsIgnoreCase("setEntrance")){
+			
+		}
+		
+		else if(args[0].equalsIgnoreCase("start")){
+			
+		}
+		
+		else if(args[0].equalsIgnoreCase("stop")){
+			
+		}
+		
+		else if(args[0].equalsIgnoreCase("newLobby")){
+			
+		}
+		
+		else if(args[0].equalsIgnoreCase("removeLobby")){
+			
+		}else{
+			sender.sendMessage("Something went wrong...");
+			sender.sendMessage("Valid commands are "+commandsToString()+"\n");
 		}
 		
 		return true;
